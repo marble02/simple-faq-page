4c2b795254b5dfc00f8285dd81b351e9e638f9ce
m <mm.marble02@gmail.com>
2022-07-04 16:33:43 -0600

    Basic faq page exercise.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Header";
+import FAQ from "./FAQ";
 
 function App() {
+  const [faqs, setfaqs] = useState(
+    [
+      {
+        question: "How many bones does a cat have?",
+        answer: "A cat has 230 bones - 6 more than a human",
+        open: true
+      },
+      {
+        question: "How much do cats sleep?",
+        answer: "The average cat sleeps 12-16 hours per day",
+        open: false
+      },
+      {
+        question: "How long do cats live?",
+        answer: "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+        open: false
+      },
+    ]
+  );
+
+  const toggleFAQ = index => {
+    setfaqs(faqs.map((faq, i) => {
+      if (i === index) {
+        faq.open = !faq.open;
+      } else {
+        faq.open = false;
+      }
+      return faq;
+    }));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className="faqs">
+        {faqs.map( (faq, i) => (
+          <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ}/>
+        )
+        )}
+      </div>
     </div>
   );
 }
